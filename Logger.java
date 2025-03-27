6e55ae283d8be2b281f8bbb8c79a95573f3663a3
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-03-27 20:44:48 -0300

    Commit Logger

---
+++ Logger.java
@@ -0,0 +1,17 @@
+public class Logger {
+
+    private static Logger instancia;
+
+    private Logger() {}
+
+    public static Logger getInstancia() {
+        if (instancia == null) {
+            instancia = new Logger();
+        }
+        return instancia;
+    }
+
+    public void log(String mensagem) {
+        System.out.println("LOG: " + mensagem);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,22 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        Endereco endereco1 = new Endereco("Rua das Floriculturas");
+        Logger logger1 = Logger.getInstancia();
+        logger1.log("Primeira mensagem de log");
 
-        Pessoa pessoa1 = new Pessoa("Raphael", endereco1);
+        Logger logger2 = Logger.getInstancia();
+        logger2.log("Segunda mensagem de log");
 
-        System.out.println("Original Pessoa: " + pessoa1);
-
-        Pessoa pessoaShallow = pessoa1.cloneShallow();
-        System.out.println("Shallow Cloned Pessoa: " + pessoaShallow);
-
-        Pessoa pessoaDeep = pessoa1.cloneDeep();
-        System.out.println("Deep Cloned Pessoa: " + pessoaDeep);
-
-        endereco1.setRua("Rua das Flores 61");
-        
-        System.out.println("apôs o Jutsu fica assim:");
-        System.out.println("Original Pessoa: " + pessoa1);
-        System.out.println("Shallow Cloned Pessoa: " + pessoaShallow);
-        System.out.println("Deep Cloned Pessoa: " + pessoaDeep);
+        System.out.println("As instâncias são iguais? " + (logger1 == logger2));
     }
 }
