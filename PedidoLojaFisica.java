f1c620f6a0a9204d2e66a9e2dc99cd98ae4ba715
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-03-27 19:27:38 -0300

    Commit pedido

---
+++ PedidoLojaFisica.java
@@ -0,0 +1,6 @@
+public class PedidoLojaFisica extends Pedido {
+  @Override
+  protected PedidoItem criarItem(Produto produto, int quantidade){
+    return new ItemPedidoLojaFisica(produto, quantidade);
+  }
+}
---
+++ PedidoOnline.java
@@ -0,0 +1,6 @@
+public class PedidoOnline extends Pedido {
+  @Override
+  protected PedidoItem criarItem(Produto produto, int quantidade) {
+    return new ItemPedidoOnline(produto, quantidade);
+  }
+}
